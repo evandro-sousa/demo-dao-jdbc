122b4de690d53e0a91cc19f4578809cf931f8115
Evandro Sousa <evandro.sousa.df@hotmail.com>
2020-09-29 21:17:19 -0300

    demo-dao-jdbc

---
+++ src/br/com/aplicacao/Programa.java
@@ -0,0 +1,15 @@
+package br.com.aplicacao;
+
+import br.com.modelo.entidade.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1, "Livros");
+		
+		System.out.println(obj);
+
+	}
+
+}
