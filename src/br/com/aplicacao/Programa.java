a7f0282026a2218ab14e71ab03c3ce823280594c
Evandro Sousa <evandro.sousa.df@hotmail.com>
2020-09-29 21:37:08 -0300

    demo-dao-jdbc

--- src/br/com/aplicacao/Programa.java
+++ src/br/com/aplicacao/Programa.java
@@ -1,6 +1,11 @@
 package br.com.aplicacao;
 
+
+
+import java.util.Date;
+
 import br.com.modelo.entidade.Departamento;
+import br.com.modelo.entidade.Vendedor;
 
 public class Programa {
 
@@ -8,7 +13,9 @@ public class Programa {
 		
 		Departamento obj = new Departamento(1, "Livros");
 		
-		System.out.println(obj);
+		Vendedor vendedor = new Vendedor(33, "Evandro", "evandro@hotmail.com"	, new Date(), 1000.00, obj);
+				
+		System.out.println(vendedor);
 
 	}
 
